535957ec7ea4c5cd313303a9abcdfc694ca35af5
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-02-01 20:23:01 +0100

    add more comments

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,8 @@
 import {configureStore} from "@reduxjs/toolkit";
-import {counterSlice} from "../slices/counterSlice";
 import {rootReducers} from "../slices";
 
+// Create the store instance in which we specify our reducers
+// In this case, its the combination of all our reducers
 export const store = configureStore({
     reducer: rootReducers
 })
